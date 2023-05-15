1e034a876678dc7f78da4ec8863ea9af0fe4d5ae
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-05-15 11:36:18 +0200

    Complete es1.2

--- es.pl
+++ es.pl
@@ -1,6 +1,12 @@
 % ES 1.1
 % search2 (Elem , List )
 % looks for two consecutive occurrences of Elem
-search2(E, [E|T]).
+search2(E, [E, E|T]).
 search2(E, [H|T]) :- search2(E, T).
+
+% ES 1.2
+% search2 (Elem , List )
+% looks for two consecutive occurrences of Elem
+search_two(E, [E, X, E|T]).
+search_two(E, [H|T]) :- search_two(E, T).
 
