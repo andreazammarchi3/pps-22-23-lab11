7d09563a0491d4a6d16e84987b3e7731ba44568b
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-05-15 11:47:41 +0200

    Complete es1.3

--- es.pl
+++ es.pl
@@ -2,11 +2,17 @@
 % search2 (Elem , List )
 % looks for two consecutive occurrences of Elem
 search2(E, [E, E|T]).
-search2(E, [H|T]) :- search2(E, T).
+search2(E, [_|T]) :- search2(E, T).
 
 % ES 1.2
-% search2 (Elem , List )
-% looks for two consecutive occurrences of Elem
+% search_two (Elem , List )
+% looks for two occurrences of Elem with any element in between !
 search_two(E, [E, X, E|T]).
-search_two(E, [H|T]) :- search_two(E, T).
+search_two(E, [_|T]) :- search_two(E, T).
 
+
+% ES 1.3
+% size (List , Size )
+% Size will contain the number of elements in List
+size([], 0).
+size([_, T], S) :- size(T, R), S is R + 1.
